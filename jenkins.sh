65d9c249306edd33aa695d3d0531a45824f62b0d
Shrathan Kumar <shrathankumar15@gmail.com>
2024-08-01 19:13:13 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -3,6 +3,7 @@
 sudo apt update -y
 sudo apt install openjdk-11-jdk -y
 sudo apt update -y
+sudo apt install git -y
 sudo apt install maven -y
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
